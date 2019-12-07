850606876764e309cf2e27cd23e27eceffbf8d7e
Sujit27 <sujit271290@gmail.com>
2019-12-07 13:12:25 +0100

    Loss criteria for discriminator during dreaming is changed from BCELoss to just negative of output value, similar to DeepDream loss. Also, list of intermediate discriminator iterations and its learning rate are used to get a range of images'

--- code/createRealisticDreams.py
+++ code/createRealisticDreams.py
@@ -1,5 +1,6 @@
 import sys
-sys.path.append('../lib/')
+#sys.path.append('../')
+#sys.path.append('../lib/')
 from trainDiscriminator import *
 
 # created by Sujit Sahoo, 30.11.2019
@@ -10,8 +11,8 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 numInitialDreamItrs = 100
 numIntermediateLoops = 20
 numIntermediateDreamItrs = 20
-numIntermediateDiscrimItrs = 10
-lrDiscriminator = 0.1
+numIntermediateDiscrimItrs_list = [1,5,10,20]
+lrDiscriminator_list = [0.01,0.05,0.1,0.2]
 label = 538 # dome
 
 def main():
@@ -24,12 +25,12 @@ def main():
     
     discriminator = createDiscriminator()
     try:
-        discriminator.load_state_dict(torch.load('discriminator.pth'))
+        discriminator.load_state_dict(torch.load(os.path.join('../','discriminator.pth')))
     except FileNotFoundError:
         print("There is no discriminator.pth file available. Maybe run trainDiscriminator.py to create it")
         raise
     discriminator.to(device)
-    criterion = nn.BCEWithLogitsLoss()
+    #criterion = nn.BCEWithLogitsLoss()
     
     desiredLabel = np.array([1.0]) # For the discriminator, label for dream and real images is 0 and 1 respectively
     desiredLabel = torch.from_numpy(desiredLabel).float().to(device)
@@ -37,26 +38,30 @@ def main():
 
     dreamTensor = dreamer(label=label,nItr=numInitialDreamItrs,lr=0.12)
 
-    for i in range(numIntermediateLoops):
-        print(f'Loop {i} out of {numIntermediateLoops}')
+    for numIntermediateDiscrimItrs in numIntermediateDiscrimItrs_list:
+        for lrDiscriminator in lrDiscriminator_list:
 
-        for j in range(numIntermediateDiscrimItrs):
+            for i in range(numIntermediateLoops):
+                print(f'Loop {i} out of {numIntermediateLoops}')
 
-            optimizer = torch.optim.SGD([dreamTensor],lrDiscriminator)
-            out = discriminator(dreamTensor)
-            loss = criterion(out,desiredLabel)
-            
-            loss.backward()
-            optimizer.step()
-            
-            ## POSSIBLY A GAUSSIAN FILTER STEP SHOULD COME HERE ???
-            dreamTensor.grad.data.zero_()
+                for j in range(numIntermediateDiscrimItrs):
+
+                    optimizer = torch.optim.SGD([dreamTensor],lrDiscriminator)
+                    out = discriminator(dreamTensor)
+                    loss = -out  #criterion(out,desiredLabel)
+                    
+                    loss.backward()
+                    optimizer.step()
+                    
+                    ## POSSIBLY A GAUSSIAN FILTER STEP SHOULD COME HERE ???
+                    dreamTensor.grad.data.zero_()
 
 
-        dreamTensor = dreamer(im=dreamTensor,label=label,nItr=numIntermediateDreamItrs,lr=0.12)
+                dreamTensor = dreamer(im=dreamTensor,label=label,nItr=numIntermediateDreamItrs,lr=0.12)
 
-    outputImage = dreamer.postProcess(dreamTensor)
-    dreamer.save(outputImage,"realDream.png")
+            outputImage = dreamer.postProcess(dreamTensor)
+            outputImageName = "realDream_"+str(numIntermediateDiscrimItrs)+"_"+str(lrDiscriminator)+".png"
+            dreamer.save(outputImage,outputImageName)
 
 if __name__ == "__main__":
     main()
